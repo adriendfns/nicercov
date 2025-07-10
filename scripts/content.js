e72bb93cf50fd6f59abc1590f03e449ee94d0595
Adrien Moreau <adrien@dfns.co>
2025-07-10 17:32:52 +0200

    fix: add debouncing

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,7 @@
+let coverageString = undefined
+let coverageFileURL = undefined
+let jsonData = undefined
+
 const extractCoverageFileURL = (coverageString) => {
   const lines = coverageString.split('\n')
   for (const line of lines) {
@@ -27,12 +31,10 @@ const renderCodeCoverageFromData = (data) => {
     for (var item of items) {
       const truc = item.children[0].children[0].children
       if (datamap[truc[truc.length - 1].children[0].title]) {
-
-
         const { coveredLines, uncoveredLines } = datamap[truc[truc.length - 1].children[0].title]
 
         const lines = item.children[1].children[0].children[2].children[2].children
-
+        if (!lines) return
         for (var line of lines) {
           if (line && line.children && line.children.length > 2) {
             const lineNumber = parseInt(line.children[2].getAttribute('data-line-number'))
@@ -50,25 +52,36 @@ const renderCodeCoverageFromData = (data) => {
   }
 }
 
-const renderCodeCoverage = () => {
-  const coverageString = getAnnotationCoverageString();
-  const coverageFileURL = extractCoverageFileURL(coverageString)
+const renderCodeCoverage = async () => {
+  if (!coverageString) {
+    coverageString = getAnnotationCoverageString();
+    if (!coverageString) return
+  }
 
-  fetch(coverageFileURL)
-    .then(async response => {
-      try {
-        const zip = new JSZip();
-        const zipContent = await zip.loadAsync(response.arrayBuffer());
-        const jsonContent = await zipContent.files['coverageDiffResult.json'].async('text');
-        const jsonData = JSON.parse(jsonContent);
-        renderCodeCoverageFromData(jsonData)
-      } catch (error) { }
-    })
-    .catch(error => { })
+  if (!coverageFileURL) {
+    coverageFileURL = extractCoverageFileURL(coverageString)
+    if (!coverageFileURL) return
+  }
+
+  if (!jsonData) {
+    const response = await fetch(coverageFileURL)
+    const zip = new JSZip();
+    const zipContent = await zip.loadAsync(response.arrayBuffer());
+    const jsonContent = await zipContent.files['coverageDiffResult.json'].async('text');
+    jsonData = JSON.parse(jsonContent);
+    console.log('download once')
+  }
+
+  renderCodeCoverageFromData(jsonData)
 }
 
+let debounceTimer = null;
+
 var obs = new MutationObserver(function (mutations, observer) {
-  renderCodeCoverage();
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(() => {
+    renderCodeCoverage();
+  }, 100); // Wait 100ms after last DOM change
 });
 
 obs.observe(document.body, { childList: true, subtree: true, attributes: false, characterData: false });
